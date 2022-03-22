b63408d32fd1f07dbd863d4ab1cd21fc1d685a58
LucienTriail <lucien.triail@gmail.com>
2022-03-22 09:45:38 +0100

    rajout de l'url products

--- backOffice/urls.py
+++ backOffice/urls.py
@@ -7,7 +7,8 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    path('transactions/',views.TransactionList.as_view())
+    path('transactions/',views.TransactionList.as_view()),
+    path('products/', views.ProductList.as_view()),
     path('products/<int:pk>', views.SingleProduct.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>', views.UserDetail.as_view()),
